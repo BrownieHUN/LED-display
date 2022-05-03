c438b5914cab2de3f034c43564347b2fe369c45e
BrownieHUN <92264890+BrownieHUN@users.noreply.github.com>
2022-05-03 11:41:35 +0200

    Improvements

--- matrix.py
+++ matrix.py
@@ -8,11 +8,15 @@ import sys #(sys.argv[x])
 # -   VARIABLES   -
 # -----------------
 
-rows = 20
+rows = 19
 columns = 95
+pieces = 6
 matrix = [["■" for i in range(columns)] for j in range(rows)] #matrix[sor][oszlop]
 options = [0,"","","",0]
 colored = "\u001b[33m■\033[0m"
+segedpixel = [0,10]
+letterdb = open("letters.txt", "r")
+letterdata = [i.strip("\n") for i in letterdb] #letterdata[sor][oszlop]   # HOGY A FASZBA LEHET EZT 18 SOROKRA OSZTANI?
 
 # -------------
 # -  NUMBERS  -
@@ -28,11 +32,54 @@ def one():
 def A():
     print("cica")
 
-#def V():
-matrix[2][2] = colored
-matrix[3][2] = colored
-matrix[2][3] = colored
-matrix[3][3] = colored
+#mostani
+#segédpixel a következőnek a helyes megjelenítése miatt kell
+def V2():
+    for i in range(len(letterdata)):
+        for j in range(len(letterdata[i])):
+            if letterdata[i][j] == "1":
+                matrix[i][j] = colored
+            segedpixel[1] += 1
+        segedpixel[0] += 1
+    segedpixel[0] = 0
+    segedpixel[1] = len(letterdata[3]) + 1
+    print(segedpixel)
+
+#előző
+def V():
+    print(segedpixel[0])
+    segedpixel[0] += segedpixel[0]+1
+    print(segedpixel[0])
+    matrix[2][2] = colored
+    matrix[3][2] = colored
+    matrix[2][3] = colored
+    matrix[3][3] = colored
+    for i in range(4, 8):
+        matrix[i][3] = colored
+        matrix[i][4] = colored
+    for i in range(8, 11):
+        matrix[i][4] = colored
+        matrix[i][5] = colored
+    for i in range(11, 15):
+        matrix[i][5] = colored
+        matrix[i][6] = colored
+    for i in range(15, 17):
+        matrix[i][6] = colored
+        matrix[i][7] = colored
+        matrix[i][8] = colored
+    for i in range(11, 15):
+        matrix[i][8] = colored
+        matrix[i][9] = colored
+    for i in range(8, 11):
+        matrix[i][9] = colored
+        matrix[i][10] = colored
+    for i in range(4, 8):
+        matrix[i][10] = colored
+        matrix[i][11] = colored
+    matrix[2][11] = colored
+    matrix[3][11] = colored
+    matrix[2][12] = colored
+    matrix[3][12] = colored
 
 # ----------------
 # -  BASH THING  -
@@ -54,8 +101,15 @@ for i in range(len(sys.argv)):
             "-h,    --help              displays this menu\n"
             )
 
-#matrix[0][0] = "\u001b[33m■\033[0m"
 
+#matrix[0][0] = "\u001b[33m■\033[0m"
+#for i in range(rows):
+#    for j in range(0, columns, 12):
+#        matrix[i][j] = colored
+
+V2()
+
+#print(matrix[1])
 for i in range(rows):
     for j in range(columns):
         print(matrix[i][j], end = " ")
